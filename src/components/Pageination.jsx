421d1e089d5c24db8a3f0ab6fa25c796662d60d4
AyanPal06 <162800916+AyanPal06@users.noreply.github.com>
2024-04-29 20:09:03 +0530

    Bug fixed

--- src/components/Pageination.jsx
+++ src/components/Pageination.jsx
@@ -5,7 +5,7 @@ const Pageination=()=>{
     const {page,handlePageChange,totalPages}=useContext(AppContext);
     return(
         <div className="w-full border-t-2 py-3  bg-white shadow-lg fixed bottom-0">
-             <div className=" w-[650px] mx-auto flex justify-between items-center">
+             <div className=" max-w-[650px] w-11/12 mx-auto flex justify-between items-center">
                 <div className="flex flex-start gap-x-3">
                      {page > 1 &&
                         <button className="rounded-md border border-gray-300 px-4 py-1" onClick={()=>handlePageChange(page-1)}>Previous</button>
